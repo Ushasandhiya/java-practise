83b4aa511985be36c5e4573fac0517b90f967868
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-08-09 10:42:37 +0530

    practise program

---
+++ name.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class name
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String Name = scan.nextLine();
+        int Age = scan.nextInt();
+        System.out.println("Name:"+Name);
+        System.out.println("Age:"+Age);
+    }
+}
