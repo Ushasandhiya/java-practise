c11631f23bcddf9cb3aefe88107f21e917ffd1d8
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-08-27 15:59:30 +0530

    exception handling

--- error.java
+++ error.java
@@ -8,6 +8,24 @@ class DivisionExample
             int result = num/den;
         }
 
-        catch
+        catch(ArithmeticException e)
+        {
+            System.out.println("Denominator cannot be zero");
+        }
+        finally{
+            System.out.println("Execution completed");
+        }
+    }
+}
+
+public class error{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        DivisionExample obj = new DivisionExample();
+        obj.dividenumbers(n1, n2);
     }
 }
