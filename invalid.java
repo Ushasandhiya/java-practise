f07687734b779cc6d38f2f7f2552bd2359559f24
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-08-27 16:12:15 +0530

    custom exception handling

--- invalid.java
+++ invalid.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class InvalidageException extends Exception
 {
     public InvalidageException(String message)
@@ -9,4 +11,26 @@ class InvalidageException extends Exception
 class Invalid
 {
     void checkage(int age)
+    {
+        try{
+            if(age<0 || age >150)
+            {
+                throw InvalidageException("age is invalid");
+            }
+
+        }
+        catch(Exceptione e){
+            System.out.println(e);
+        }
+    }
+}
+
+public class invalid{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int age = scan.nextInt();
+
+        invalid a1 = new invalid();
+        a1.checkage(age);
+    }
 }
