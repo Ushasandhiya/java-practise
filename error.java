e68cbd7946d998fd9a72d75da657097b2a37feb6
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-08-27 15:55:54 +0530

    exception handling

---
+++ error.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class DivisionExample
+{
+    void dividenumbers(int num,int den)
+    {
+        try{
+            int result = num/den;
+        }
+
+        catch
+    }
+}
