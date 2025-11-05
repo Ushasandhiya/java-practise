b7280939b905af61482b1ad0931e2deb8d16dfcd
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-11-05 13:17:59 +0530

    practise leetcode

--- ran.java
+++ ran.java
@@ -6,6 +6,19 @@ public class ran {
         Scanner sc = new Scanner(System.in);
         int n = scan.nextInt();
 
+        for(int i>1;i<=n;i++){
+            if(n%3==0){
+                
+                System.out.println("fizz");
+            }
+            else if(n%5==0){
+                System.out.println("buzz");
+            }
+            else if(n%3==0 && n%5==0){
+                System.out.println("fizzbuzz");
+            }
+        }
+
     }
     
 }
