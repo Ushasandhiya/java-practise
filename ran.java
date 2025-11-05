a3169f3067ce4ca2591e3a66cc9b3a40bba9fa8e
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-11-05 13:11:42 +0530

    practise leetcode

---
+++ ran.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ran {
+
+    Public static void main(String args[]){
+        System.out.println("enter a number:");
+        Scanner sc = new Scanner(System.in);
+        int n = scan.nextInt();
+
+    }
+    
+}
