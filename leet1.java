bc36b815d587d109925d507d3570affa7fdf2eaa
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-11-06 10:57:05 +0530

    practise leetcode

--- leet1.java
+++ leet1.java
@@ -6,13 +6,15 @@ public static void main(String args[]){
 Scanner sc = new Scanner (System.in);
 System.out.println("num:");
 int num = sc.nextInt();
+int count = 0;
 while(num!=0){
 if(num%2==0){
 num = num/2;
 }else{
 num = num-1;
+}
+count++;
+}
 System.out.println(num);
-}
-}
 }
 }
